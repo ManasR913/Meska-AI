3bd07014149f9f39e797734e0faf2b6979809ec7
ManasR913 <manas.rajendran913@gmail.com>
2025-03-17 20:59:42 -0500

    3-17 implemented explicit wait for links and link collection

--- src/drHorton_seleniumScraper.py
+++ src/drHorton_seleniumScraper.py
@@ -3,7 +3,9 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import pandas as pd
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# import pandas as pd
 # import ace_tools as tools
 import time
 
@@ -13,16 +15,24 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.drhorton.com//texas")
 
-# explicit wait for links to  show up
+# explicit wait for links to show up
 try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    link_elements = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.XPATH, "//div[contains(@class, 'home-info__available-homes')]/object/a"))
     )
-finally:
-    driver.quit()
+    print(f"✅ Link found: {len(link_elements)} total links on this page...")
+
+    # we want to extract the links themselves into a separate list make a new list called linksAH
+    available_home_links = [element.get_attribute("href") for element in link_elements]
+except: 
+    print(f"Links not found")
 
 #get webpage title
 print(driver.title)
+
+print(link_elements[0])
+
+# we want to extract the links themselves into a separate list make a new list called linksAH
 
 
 
